11259bb4a7903bd4780f0b61b8a165570435434f
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-04-14 10:48:55 +0700

    menambahkan fungsi bubble sort ascending

--- BinarySearch_.cpp
+++ BinarySearch_.cpp
@@ -24,8 +24,27 @@ void input()
     }
 }
 
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+}
+
 int main()
 {
     input();
+    bubbleSortArray();
     return 0;
 }
